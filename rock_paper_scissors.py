176d99718630c4fd7fe391e600b1425a9178c92d
Kei Thoma <thomakmj@gmail.com>
2026-05-06 01:23:04 +0200

    bit hard

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,3 +4,14 @@ print("")
 value_if_true1 if condition1 else value_if_true2 if condition2 else value_if_false
 
 lambda y, m: 
+
+print(
+    (
+        L := lambda y, m:
+            (u := input("stone / paper / scissors: ").lower()) and
+            (m := ("stone", "paper", "scissors")[id(object) % 3]) and
+            (
+                print()
+
+
+)
